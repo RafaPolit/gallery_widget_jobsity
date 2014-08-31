5a3c9687a40cd0ec3e424e837ce7ad7b2a800e26
RafaPolit <rafael.polit@provedatos.com>
2014-08-31 00:08:38 -0500

    Main image is implemented and tested

--- scripts/galleryWidget.js
+++ scripts/galleryWidget.js
@@ -1,5 +1,38 @@
 'use strict';
 
+var galleryWidget = {
+
+  widgets: [],
+
+  initializeWidgets: function() {
+    this.widgets = document.getElementsByClassName('gallery-widget');
+    for(var index = 0; index < this.widgets.length; index ++) {
+      var mainImageContainer = createMainImageContainer(this.widgets[index]);
+      this.widgets[index].appendChild(mainImageContainer);
+    }
+  }
+
+};
+
+window.addEventListener('load', galleryWidget.initializeWidgets);
+
+function createMainImageContainer(widget) {
+  var mainImageContainer = document.createElement('div');
+  mainImageContainer.className =  'main-image-container';
+
+  var mainImage = document.createElement('div');
+  mainImage.className = 'main-image enter-from-right';
+  mainImageContainer.appendChild(mainImage);
+
+  var img = document.createElement('img');
+  img.setAttribute('src', widget.getElementsByTagName('img')[0].getAttribute('src'));
+  mainImage.appendChild(img);
+
+  return mainImageContainer;
+}
+
+
+
 function enter() {
   var mainImage = document.getElementsByClassName('main-image');
   console.log(mainImage);
--- specs/galleryWidgetSpec.js
+++ specs/galleryWidgetSpec.js
@@ -1,9 +1,44 @@
+/* global galleryWidget */
 'use strict';
+
+/* ---------------------------------------------------------- */
+/* hook to allow deferred scripts to run after initialization */
+window.removeEventListener('load', galleryWidget.initializeWidgets);
+document.addEventListener('DOMContentLoaded', function() {
+  galleryWidget.initializeWidgets();
+});
+/* ---------------------------------------------------------- */
 
 describe('Gallery Widget', function() {
 
-  it('should initialize widgets on document load', function() {
-    expect(widgets.length).toBe(2);
+  var thumbnailWidget;
+  var singleWidget;
+
+  beforeEach(function() {
+    thumbnailWidget = galleryWidget.widgets[0];
+    singleWidget = galleryWidget.widgets[1];
+  });
+
+  describe('on Initialization', function() {
+    it('should assign the widgets array with the different elements containing class', function() {
+      expect(galleryWidget.widgets.length).toBe(2);
+    });
+
+    describe('Main Image', function() {
+
+      it('should append the main image container structure into widgets of both modes', function() {
+        expect(thumbnailWidget.getElementsByClassName('main-image-container').length).toBe(1);
+        expect(thumbnailWidget.getElementsByClassName('main-image').length).toBe(1);
+
+        var mainImage = thumbnailWidget.getElementsByClassName('main-image')[0];
+        expect(mainImage.className).toBe('main-image enter-from-right');
+        expect(mainImage.getElementsByTagName('img').length).toBe(1);
+        expect(mainImage.getElementsByTagName('img')[0].getAttribute('src')).toBe('images/Barcelona_MNAC_9671.jpg');
+
+        expect(singleWidget.getElementsByClassName('main-image-container').length).toBe(1);
+      });
+
+    });
   });
 
 });
